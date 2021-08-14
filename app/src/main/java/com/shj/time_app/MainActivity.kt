5c0cb5efd39657c595e4f5f19f69902e4831be75
HojuShin <shin03188@naver.com>
2021-08-14 16:13:55 +0900

    프래그먼트 화면 전환 성공

--- app/src/main/java/com/shj/time_app/MainActivity.kt
+++ app/src/main/java/com/shj/time_app/MainActivity.kt
@@ -2,10 +2,45 @@ package com.shj.time_app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigationView);
+        loadFragment(TodayFragment())
+
+        bottomNavigationView.setOnNavigationItemSelectedListener { MenuItem ->
+            when {
+                MenuItem.itemId == R.id.navigationToday -> {
+                    loadFragment(TodayFragment())
+                    return@setOnNavigationItemSelectedListener true
+                }
+                MenuItem.itemId == R.id.navigationHome -> {
+                    loadFragment(HomeFragment())
+                    return@setOnNavigationItemSelectedListener true
+                }
+                MenuItem.itemId == R.id.navigationNote -> {
+                    loadFragment(NoteFragment())
+                    return@setOnNavigationItemSelectedListener true
+                }
+                else -> {
+                    return@setOnNavigationItemSelectedListener  false
+                }
+            }
+        }
+
+    }
+
+    private fun loadFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction().also { fragmentTransaction ->
+            fragmentTransaction.replace(R.id.FragmentContainer, fragment)
+            fragmentTransaction.commit()
+        }
     }
 }
